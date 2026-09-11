42c7a529cd29fcfef75f5faecd07a1ef098472d1
Michael <106012217+mikecity11@users.noreply.github.com>
2026-09-11 14:03:26 +0100

    Update genlayer.ts

--- lib/genlayer.ts
+++ lib/genlayer.ts
@@ -2,7 +2,7 @@
 "use client";
 
 import { createClient } from "genlayer-js";
-import { studionet } from "genlayer-js/chains";
+import { localnet } from "genlayer-js/chains";
 import {
   TransactionHashVariant,
   TransactionStatus,
